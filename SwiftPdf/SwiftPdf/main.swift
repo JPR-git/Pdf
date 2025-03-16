7fd12d670a299f7740898ec3a9a2557dd2a05923
JPR <jan@pruner.dev>
2025-03-16 12:17:41 +0100

    Init

--- SwiftPdf/SwiftPdf/main.swift
+++ SwiftPdf/SwiftPdf/main.swift
@@ -6,6 +6,19 @@
 //
 
 import Foundation
+import PDFKit
 
 print("Hello, World!")
+
+let inputFileURL = FileManager.default.homeDirectoryForCurrentUser.appending(
+    path: "Downloads/Account_statement_0-3161711093_from_20250131.pdf")
+
+let pdf = PDFDocument(url: inputFileURL)!
+
+print(pdf.documentAttributes)
+
+let page0 = pdf.page(at: 0)
+let string = page0?.string
+
+print(string)
 
