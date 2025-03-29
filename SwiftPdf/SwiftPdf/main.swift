6ab275e7a4aebf4c425af16f8885695e81a8efd2
JPR <jan@pruner.dev>
2025-03-29 16:18:44 +0100

    All pdf files with pattern...

--- SwiftPdf/SwiftPdf/main.swift
+++ SwiftPdf/SwiftPdf/main.swift
@@ -1,24 +1,39 @@
-//
-//  main.swift
-//  SwiftPdf
-//
-//  Created by Jan on 16.03.2025.
-//
-
 import Foundation
 import PDFKit
 
-print("Hello, World!")
+let directoryURL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
+let fileNamePattern: String = "Account_statement_"
 
-let inputFileURL = FileManager.default.homeDirectoryForCurrentUser.appending(
-    path: "Downloads/Account_statement_0-3161711093_from_20250131.pdf")
 
-let pdf = PDFDocument(url: inputFileURL)!
-
-print(pdf.documentAttributes)
-
-let page0 = pdf.page(at: 0)
-let string = page0?.string
-
-print(string)
-
+var dirItems = try FileManager.default.contentsOfDirectory(atPath: directoryURL.absoluteURL.path())
+for item in dirItems
+{
+    if item.hasPrefix(fileNamePattern) && item.hasSuffix(".pdf") {
+        print(item)
+        let inputFileURL = URL(fileURLWithPath: item, relativeTo: directoryURL)
+        
+        guard let pdf = PDFDocument(url: inputFileURL) else {
+            continue
+        }
+        
+        let fileURL = URL(fileURLWithPath: inputFileURL.absoluteString, relativeTo: directoryURL).appendingPathExtension("txt")
+        
+        if FileManager.default.createFile(atPath: fileURL.absoluteURL.path(), contents: nil) {
+            
+            let fHandle = try FileHandle.init(forWritingTo: fileURL)
+            try fHandle.seekToEnd()
+            
+            for i in (0...pdf.pageCount) {
+                do {
+                    try fHandle.write(contentsOf: (pdf.page(at: i)?.string ?? "").data(using: .utf8)!)
+                }
+                catch {
+                    print(error)
+                }
+            }
+            try fHandle.close()
+        } else {
+            print("Output file not created.")
+        }
+    }
+}
